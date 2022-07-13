f94dae6d1c6912dd3a109fd15e3deb604e0e365b
Kevin Heruer <k.heruer@gmail.com>
2022-07-13 23:51:05 +0200

    Added docs, fixed lint issue, ran formatter

--- islands/Countdown.tsx
+++ islands/Countdown.tsx
@@ -4,26 +4,31 @@ import { useEffect, useState } from "preact/hooks";
 
 const timeFmt = new Intl.RelativeTimeFormat("nl-NL");
 
+/**
+ * Creates a countdown based on given target.
+ * @param props Properties to set
+ * @returns Default handler
+ */
 export default function Countdown(props: { target: string }) {
-    const target = new Date(props.target);
-    const [now, setNow] = useState(new Date());
+  const target = new Date(props.target);
+  const [now, setNow] = useState(new Date());
 
-    useEffect(() => {
-        const timer = setInterval(() => {
-            setNow((now) => {
-                if (now > target) {
-                    clearInterval(timer);
-                }
-                return new Date();
-            });
-        }, 1000);
-        return () => clearInterval(timer);
-    }, [props.target]);
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setNow((now) => {
+        if (now > target) {
+          clearInterval(timer);
+        }
+        return new Date();
+      });
+    }, 1000);
+    return () => clearInterval(timer);
+  }, [props.target]);
 
-    if (now > target) {
-        return <span>Tada</span>;
-    }
+  if (now > target) {
+    return <span>Tada</span>;
+  }
 
-    const secondsLeft = Math.floor((target.getTime() - now.getTime()) / 1000);
-    return <span>{timeFmt.format(secondsLeft, "seconds")}</span>;
+  const secondsLeft = Math.floor((target.getTime() - now.getTime()) / 1000);
+  return <span>{timeFmt.format(secondsLeft, "seconds")}</span>;
 }
--- routes/countdown.tsx
+++ routes/countdown.tsx
@@ -1,13 +1,13 @@
 /** @jsx h */
 import { h } from "preact";
-import Countdown from "../islands/Countdown.tsx"
+import Countdown from "../islands/Countdown.tsx";
 
 export default function Page() {
-    const date = new Date();
-    date.setSeconds(date.getSeconds() + 5);
-    return (
-        <p>
-            The big even is happening <Countdown target={date.toISOString()} />.
-        </p>
-    );
+  const date = new Date();
+  date.setSeconds(date.getSeconds() + 5);
+  return (
+    <p>
+      The big even is happening <Countdown target={date.toISOString()} />.
+    </p>
+  );
 }
